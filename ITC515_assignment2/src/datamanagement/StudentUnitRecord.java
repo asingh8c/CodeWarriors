cc6f709477a9cd1c26b919ae8e1c3b795ad1e7c1
Asmita <asmitasingh15@gmail.com>
2016-08-16 23:59:45 +1000

    Naming Conventions followed and changes made,Comments Added, Code Indentation corrected.

--- ITC515_assignment2/src/datamanagement/StudentUnitRecord.java
+++ ITC515_assignment2/src/datamanagement/StudentUnitRecord.java
@@ -3,67 +3,98 @@ package datamanagement;
  * Class to store and retrieve Student Id, Unit code, Assignment marks and final exam marks
  */
 public class StudentUnitRecord implements IStudentUnitRecord {
-	private Integer sid;
-	private String uc;
-	private float a1, a2, ex;
+	private Integer studentId;	//Attribute variable to store Student Id
+	private String unitCode;	//Attribute variable to store Unit Code
+	private float asg1Marks, asg2Marks, examMarks;	//Attribute variable to store assignment marks and exam marks
 
-	public StudentUnitRecord(Integer id, String code, float asg1, float asg2,
-			float exam) {
-		this.sid = id;
-		this.uc = code;
-		this.setAsg1(asg1);
-		this.setAsg2(asg2);
-		this.setExam(exam);
+	/*
+	 * Class constructor to initialize class attributes with values passed as argument
+	 * @param id student id
+	 * @param code	unit code
+	 * @param asg1 assignment 1 marks
+	 * @param asg2 assignment 2 marks
+	 * @param exam final exam marks 
+	 */
+	public StudentUnitRecord(Integer id, String code, float asg1, float asg2,float exam) {
+		this.studentId = id;
+		this.unitCode = code;
+		this.setAsg1Marks(asg1);
+		this.setAsg2Marks(asg2);
+		this.setExamMarks(exam);
 	}
-
-	public Integer getStudentID() {
-		return sid;
+	/*
+	 * (non-Javadoc)
+	 * @see datamanagement.IStudentUnitRecord#getStudentId()
+	 */
+	public Integer getStudentId() {
+		return studentId;
 	}
+	/*
+	 * (non-Javadoc)
+	 * @see datamanagement.IStudentUnitRecord#getUnitCode()
+	 */
 
 	public String getUnitCode() {
-		return uc;
+		return unitCode;
 	}
-
-	public void setAsg1(float a1) {
-		if (a1 < 0 ||
-			a1 > UnitManager.UM().getUnit(uc).getAsg1Weight()) {
+	/*
+	 * (non-Javadoc)
+	 * @see datamanagement.IStudentUnitRecord#setAsg1Marks(float)
+	 */
+	public void setAsg1Marks(float a1) {
+		if (a1 < 0 ||a1 > UnitManager.UM().getUnit(unitCode).getAsg1Weight()) {
 			throw new RuntimeException("Mark cannot be less than zero or greater than assessment weight");
 		}
-		this.a1 = a1;
+		this.asg1Marks = a1;
 	}
+	/*
+	 * (non-Javadoc)
+	 * @see datamanagement.IStudentUnitRecord#getAsg1Marks()
+	 */
 
-	public float getAsg1() {
-
-		return a1;
+	public float getAsg1Marks() {
+		return asg1Marks;
 	}
-
-	public void setAsg2(float a2) {
-		if (a2 < 0 ||
-			a2 > UnitManager.UM().getUnit(uc).getAsg2Weight()) {
+	/*
+	 * (non-Javadoc)
+	 * @see datamanagement.IStudentUnitRecord#setAsg2Marks(float)
+	 */
+	public void setAsg2Marks(float a2) {
+		if (a2 < 0 || a2 > UnitManager.UM().getUnit(unitCode).getAsg2Weight()) {
 			throw new RuntimeException("Mark cannot be less than zero or greater than assessment weight");
 		}
-		this.a2 = a2;
-
+		this.asg2Marks = a2;
 	}
-
-	public float getAsg2() {
-		return a2;
+	/*
+	 * (non-Javadoc)
+	 * @see datamanagement.IStudentUnitRecord#getAsg2Marks()
+	 */
+	public float getAsg2Marks() {
+		return asg2Marks;
 	}
-
-	public void setExam(float ex) {
-		if (ex < 0 ||
-				ex > UnitManager.UM().getUnit(uc).getExamWeight()) {
+	/*
+	 * (non-Javadoc)
+	 * @see datamanagement.IStudentUnitRecord#setExamMarks(float)
+	 */
+	public void setExamMarks(float ex) {
+		if (ex < 0 || ex > UnitManager.UM().getUnit(unitCode).getExamWeight()) {
 				throw new RuntimeException("Mark cannot be less than zero or greater than assessment weight");
 			}
-		this.ex = ex;
+		this.examMarks = ex;
 	}
-
-	public float getExam() {
-		return ex;
+	/*
+	 * (non-Javadoc)
+	 * @see datamanagement.IStudentUnitRecord#getExamMarks()
+	 */
+	public float getExamMarks() {
+		return examMarks;
 	}
-
-	public float getTotal() {
-		return a1 + a2 + ex;
+	/*
+	 * (non-Javadoc)
+	 * @see datamanagement.IStudentUnitRecord#getTotalMarks()
+	 */
+	public float getTotalMarks() {
+		return asg1Marks + asg2Marks + examMarks;
 
 	}
 }
