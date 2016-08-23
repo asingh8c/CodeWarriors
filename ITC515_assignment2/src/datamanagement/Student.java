541c47048d001bd8ed716578132b8369ae570fab
Krishanthi <Krishanthi@192.168.0.4>
2016-08-23 22:13:24 +1000

    As we discussed in the meeting I used camelCase for the variables called fn=firtName ln=lastName Make inLine comments in suitable places Make parentheses correctly

--- ITC515_assignment2/src/datamanagement/Student.java
+++ ITC515_assignment2/src/datamanagement/Student.java
@@ -1,37 +1,47 @@
 package datamanagement;
 
 public class Student implements IStudent {
-    private Integer id; private String fn;
-            private String ln;
-private StudentUnitRecordList su;
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private StudentUnitRecordList su;
 
-public Student( Integer id, String fn, String ln, StudentUnitRecordList su ) { this.id = id; this.fn = fn;
-        this.ln = ln;this.su = 
+public Student( Integer id, String firstName, String ln, StudentUnitRecordList su ) { 
+	this.id = id; //declare variable for student id
+	this.firstName = firstName;//declare variable for first name
+    this.lastName = lastName;//declare variable for last name
+    this.su = 
         su == null ? new StudentUnitRecordList() : 
                 su;
 }
-
-    public Integer getID() { return this.id; 
-} public String getFirstName() { 
-return fn; }
-
+//Declaration of function to get student Id
+    public Integer getID() {
+    	return this.id; 
+} 
+//Declaration of function to get student first name
+    public String getFirstName() { 
+    	return firstName; 
+    	}
+//Declaration of function to set the student first name
     public void setFirstName( String firstName ) { 
-this.fn = firstName; }
-
-public String getLastName() { 
-    return ln; }
+    	this.firstName = firstName; 
+}
+//Declaration of function to get the student last name
+    public String getLastName() { 
+    	return lastName; 
+    }
+//Declaration of function to set the student last name
     public void setLastName( String lastName ) { 
-
-        
-this.ln = lastName; }
+    	this.lastName = lastName;
+    	}
 
 public void addUnitRecord( IStudentUnitRecord record ) { su.add(record); }
         public IStudentUnitRecord getUnitRecord( String unitCode ) {
-for ( IStudentUnitRecord r : su ) 
+        	for ( IStudentUnitRecord r : su ) 
             if ( r.getUnitCode().equals(unitCode)) 
-return r; 
+            	return r; 
 
-return null;
+        	return null;
         
 }
 
