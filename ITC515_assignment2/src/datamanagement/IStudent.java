f34a69d5b6562c66f7b7fb846853e65af008ec76
thilinip77 <thilinip77@yahoo.com>
2016-08-25 21:32:38 +1000

    I changed variable name getID to getid and also addheader comments and several comments for variable declaration

--- ITC515_assignment2/src/datamanagement/IStudent.java
+++ ITC515_assignment2/src/datamanagement/IStudent.java
@@ -1,18 +1,20 @@
+//It is a java programme for data management including several variables
+
 package datamanagement;
 
 public interface IStudent {
 
-	public Integer getID();
+	public Integer getId(); 
 
 	public String getFirstName();
 
-	public void setFirstName(String firstName);
+	public void setFirstName(String firstName); //static method example 1
 
 	public String getLastName();
 
-	public void setLastName(String lastName);
+	public void setLastName(String lastName);//static method example2
 
-	public void addUnitRecord(IStudentUnitRecord record);
+	public void addUnitRecord(IStudentUnitRecord record);//static method example3
 
 	public IStudentUnitRecord getUnitRecord(String unitCode);
 
