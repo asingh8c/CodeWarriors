b87d394028f59cb58f4bcbf8849aaecb9b2faf6b
thilinip77 <thilinip77@yahoo.com>
2016-08-28 15:03:30 +1000

    comment

--- ITC515_assignment2/src/datamanagement/IStudent.java
+++ ITC515_assignment2/src/datamanagement/IStudent.java
@@ -1,20 +1,19 @@
-//It is a java programme for data management including several variables
-
+//get and set a person's ID, First name, Last name to the data management apllication
 package datamanagement;
 
 public interface IStudent {
 
-	public Integer getId(); 
+	public Integer getId(); //variable declaration for getid
 
-	public String getFirstName();
+	public String getFirstName();//variable declaration for get first name
 
-	public void setFirstName(String firstName); //static method example 1
+	public void setFirstName(String firstName); //
 
-	public String getLastName();
+	public String getLastName();//variable declaration for get last name
 
-	public void setLastName(String lastName);//static method example2
+	public void setLastName(String lastName); //set Last name
 
-	public void addUnitRecord(IStudentUnitRecord record);//static method example3
+	public void addUnitRecord(IStudentUnitRecord record);
 
 	public IStudentUnitRecord getUnitRecord(String unitCode);
 
